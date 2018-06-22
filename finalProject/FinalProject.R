cfa964fde6623b2c921fd6251c90d726e14d3bce
itamarsti <itamarsti@gmail.com>
2018-06-22 15:37:08 +0300

    project file

---
+++ finalProject/FinalProject.R
@@ -0,0 +1,12 @@
+
+install.packages('ggplot2')
+install.packages('lubridate')
+install.packages(c("FactoMineR", "factoextra"));
+install.packages("dummies");
+
+
+library(ggplot2)
+library(lubridate)
+library("FactoMineR")
+library("factoextra");
+library(dummies)
