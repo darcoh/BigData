0275475f741df5d1cab8a406a2035007c56c727a
darcoh <darcoh1993@gmail.com>
2018-06-23 12:22:22 +0300

    gf

--- finalProject/FinalProject.R
+++ finalProject/FinalProject.R
@@ -59,14 +59,20 @@ summary(airbnbTrain_df[,age])
 
 
 
-#-----------merging signup_app categories------------------------
+#-----------merging signup_app categories on Train file------------------------
 airbnbTrain_df = airbnbTrain_df[,signup_app:= ifelse(signup_app=="iOS", "Mobile", signup_app)]
 airbnbTrain_df = airbnbTrain_df[,signup_app:= ifelse(signup_app=="Android", "Mobile", signup_app)]
 airbnbTrain_df = airbnbTrain_df[,signup_app:= ifelse(signup_app=="Moweb", "Mobile", signup_app)]
 
 ggplot(airbnbTrain_df, aes(signup_app)) + stat_count(color="blue",geom = "bar")
 
-#-----------merging first_device_type categories------------------------
+#-----------merging signup_app categorieson Test file------------------------
+
+airbnbTest_df = airbnbTest_df[,signup_app:= ifelse(signup_app=="iOS", "Mobile", signup_app)]
+airbnbTest_df = airbnbTest_df[,signup_app:= ifelse(signup_app=="Android", "Mobile", signup_app)]
+airbnbTest_df = airbnbTest_df[,signup_app:= ifelse(signup_app=="Moweb", "Mobile", signup_app)]
+
+#-----------merging first_device_type categories on Train file------------------------
 airbnbTrain_df = airbnbTrain_df[,first_device_type:= ifelse(first_device_type=="iPad", "Tablet", first_device_type)]
 airbnbTrain_df = airbnbTrain_df[,first_device_type:= ifelse(first_device_type=="Android Tablet", "Tablet", first_device_type)]
 airbnbTrain_df = airbnbTrain_df[,first_device_type:= ifelse(first_device_type=="Android Phone", "Tablet", first_device_type)]
@@ -74,10 +80,69 @@ airbnbTrain_df = airbnbTrain_df[,first_device_type:= ifelse(first_device_type=="
 airbnbTrain_df = airbnbTrain_df[,first_device_type:= ifelse(first_device_type=="SmartPhone (Other)", "SmartPhone", first_device_type)]
 airbnbTrain_df = airbnbTrain_df[,first_device_type:= ifelse(first_device_type=="Mac Desktop", "Desktop", first_device_type)]
 airbnbTrain_df = airbnbTrain_df[,first_device_type:= ifelse(first_device_type=="Desktop (Other)", "Desktop", first_device_type)]
+
 airbnbTrain_df = airbnbTrain_df[,first_device_type:= ifelse(first_device_type=="Windows Desktop", "Desktop", first_device_type)]
-airbnbTrain_df = airbnbTrain_df[,first_device_type:= ifelse(first_device_type=="Other/Unknown", "NA", first_device_type)]
+airbnbTrain_df = airbnbTrain_df[,first_device_type:= ifelse(first_device_type=="Other/Unknown", "na", first_device_type)]
 
 ggplot(airbnbTrain_df, aes(first_device_type)) + stat_count(color="blue",geom = "bar")
+
+#-----------merging first_device_type categories on Test file------------------------
+airbnbTest_df = airbnbTest_df[,first_device_type:= ifelse(first_device_type=="iPad", "Tablet", first_device_type)]
+airbnbTest_df = airbnbTest_df[,first_device_type:= ifelse(first_device_type=="Android Tablet", "Tablet", first_device_type)]
+airbnbTest_df = airbnbTest_df[,first_device_type:= ifelse(first_device_type=="Android Phone", "Tablet", first_device_type)]
+airbnbTest_df = airbnbTest_df[,first_device_type:= ifelse(first_device_type=="iPhone", "SmartPhone", first_device_type)]
+airbnbTest_df = airbnbTest_df[,first_device_type:= ifelse(first_device_type=="SmartPhone (Other)", "SmartPhone", first_device_type)]
+airbnbTest_df = airbnbTest_df[,first_device_type:= ifelse(first_device_type=="Mac Desktop", "Desktop", first_device_type)]
+airbnbTest_df = airbnbTest_df[,first_device_type:= ifelse(first_device_type=="Desktop (Other)", "Desktop", first_device_type)]
+airbnbTest_df = airbnbTest_df[,first_device_type:= ifelse(first_device_type=="Windows Desktop", "Desktop", first_device_type)]
+airbnbTest_df = airbnbTest_df[,first_device_type:= ifelse(first_device_type=="Other/Unknown", "na", first_device_type)]
+
+
+#---------------------------------------------------------------------------
+#---------------------------affiliate_channel-------------------------------
+#---------------------------------------------------------------------------
+airbnbTrain_df = airbnbTrain_df[,affiliate_channel:= ifelse(affiliate_channel=="other", "na", affiliate_channel)]
+
+airbnbTest_df = airbnbTest_df[,affiliate_channel:= ifelse(affiliate_channel=="other", "na", affiliate_channel)]
+
+
+#---------------------------------------------------------------------------
+#---------------------------affiliate_provider-------------------------------
+#---------------------------------------------------------------------------
+airbnbTrain_df = airbnbTrain_df[,affiliate_provider:= ifelse(affiliate_provider=="other", "na", affiliate_provider)]
+
+
+airbnbTest_df = airbnbTest_df[,affiliate_provider:= ifelse(affiliate_provider=="other", "na", affiliate_provider)]
+
+
+
+#---------------------------------------------------------------------------
+#---------------------------affiliate_first_tracked-------------------------------
+#---------------------------------------------------------------------------
+
+##this one doest work:
+airbnbTrain_df = airbnbTrain_df[,first_affiliate_tracked:= ifelse(first_affiliate_tracked==" ", "na", first_affiliate_tracked)]
+
+airbnbTest_df = airbnbTest_df[,first_affiliate_tracked:= ifelse(first_affiliate_tracked==" ", "na", first_affiliate_tracked)]
+
+#---------------------------------------------------------------------------
+#---------------------------signup_app--------------------------------------
+#---------------------------------------------------------------------------
+airbnbTrain_df = airbnbTrain_df[,first_affiliate_tracked:= ifelse(first_affiliate_tracked==" ", "na", signup_app)]
+
+
+#---------------------------------------------------------------------------
+#---------------------------first_device_type-------------------------------
+#---------------------------------------------------------------------------
+airbnbTrain_df = airbnbTrain_df[,first_affiliate_tracked:= ifelse(first_device_type=="Other/Unknown", "na", first_device_type)]
+airbnbTest_df = airbnbTest_df[,first_device_type:= ifelse(first_device_type=="Other/Unknown", "na", first_device_type)]
+
+
+#---------------------------------------------------------------------------
+#---------------------------first_browser-------------------------------
+#---------------------------------------------------------------------------
+airbnbTrain_df = airbnbTrain_df[,first_browser:= ifelse(first_browser=="-unknown-", "na", first_browser)]
+airbnbTest_df = airbnbTest_df[,first_browser:= ifelse(first_browser=="-unknown-", "na", first_browser)]
 
 #--------------converting features to categorial---------------------------
 airbnbTrain_df$affiliate_channel = factor(airbnbTrain_df$affiliate_channel)
@@ -87,18 +152,60 @@ airbnbTrain_df$signup_app = factor(airbnbTrain_df$signup_app)
 airbnbTrain_df$first_device_type = factor(airbnbTrain_df$first_device_type)
 airbnbTrain_df$first_browser = factor(airbnbTrain_df$first_browser)
 
+airbnbTest_df$affiliate_channel = factor(airbnbTest_df$affiliate_channel)
+airbnbTest_df$affiliate_provider = factor(airbnbTest_df$affiliate_provider)
+airbnbTest_df$first_affiliate_tracked = factor(airbnbTest_df$first_affiliate_tracked)
+airbnbTest_df$signup_app = factor(airbnbTest_df$signup_app)
+airbnbTest_df$first_device_type = factor(airbnbTest_df$first_device_type)
+airbnbTest_df$first_browser = factor(airbnbTest_df$first_browser)
+
+
 #----------------------------------------------------------------------------
 #---------------------------language-----------------------------------------
 #----------------------------------------------------------------------------
+sum(is.na(airbnbTrain_df$language))
 
-#---------------Counting NA----------------------------------
-
+ggplot(airbnbTrain_df, aes(language)) + stat_count(color="blue",geom = "bar")
 
 #---------------------------------------------------------------------------
 #---------------------------affiliate_channel-------------------------------
 #---------------------------------------------------------------------------
+sum(is.na(airbnbTrain_df$affiliate_channel))
 
-
+ggplot(airbnbTrain_df, aes(affiliate_channel)) + stat_count(color="blue",geom = "bar")
 #---------------------------------------------------------------------------
 #---------------------------affiliate_provider-------------------------------
 #---------------------------------------------------------------------------
+table (airbnbTrain_df$affiliate_provider)
+sum(is.na(airbnbTrain_df$affiliate_provider))
+
+ggplot(airbnbTrain_df, aes(affiliate_provider)) + stat_count(color="blue",geom = "bar")
+
+#---------------------------------------------------------------------------
+#---------------------------first_affiliate_tracked------------------------
+#---------------------------------------------------------------------------
+sum(is.na(airbnbTrain_df$first_affiliate_tracked))
+
+ggplot(airbnbTrain_df, aes(first_affiliate_tracked)) + stat_count(color="blue",geom = "bar")
+
+ggplot(airbnbTest_df, aes(first_affiliate_tracked)) + stat_count(color="blue",geom = "bar")
+#---------------------------------------------------------------------------
+#---------------------------signup_app--------------------------------------
+#---------------------------------------------------------------------------
+sum(is.na(airbnbTrain_df$signup_app))
+
+ggplot(airbnbTrain_df, aes(signup_app)) + stat_count(color="blue",geom = "bar")
+#---------------------------------------------------------------------------
+#---------------------------first_device_type-------------------------------
+#---------------------------------------------------------------------------
+ggplot(airbnbTrain_df, aes(first_device_type)) + stat_count(color="blue",geom = "bar")
+
+#---------------------------------------------------------------------------
+#---------------------------first_browser-------------------------------
+#---------------------------------------------------------------------------
+ggplot(airbnbTrain_df, aes(first_browser)) + stat_count(color="blue",geom = "bar")
+
+table(airbnbTrain_df$first_browser)
+cor(data.frame(airbnbTrain_df$country_destination,airbnbTrain_df$gender,airbnbTrain_df$age,airbnbTrain_df$signup_method,airbnbTrain_df$signup_flow, airbnbTrain_df$language, airbnbTrain_df$affiliate_channel, airbnbTrain_df$affiliate_provider, airbnbTrain_df$first_affiliate_tracked, airbnbTrain_df$signup_app, airbnbTrain_df$first_device_type, airbnbTrain_df$first_browser))
+
+    
